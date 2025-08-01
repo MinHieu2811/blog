820722c981492c228d3d4b844bdd724a5e353ce2
MinHieu2811 <cuunhatnhat51@gmail.com>
2025-08-01 21:22:40 +0700

    update footer

--- src/components/custom/Footer.tsx
+++ src/components/custom/Footer.tsx
@@ -3,27 +3,29 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="justify-between items-center p-4 max-w-6xl m-auto flex flex-wrap gap-4 mt-10">
-      <div className="logo flex items-center">
-        <Link href="/">
-          <span className="mr-5">Doggo.dev</span>
-        </Link>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <Link href="/">
-          <span className="mr-5">About</span>
-        </Link>
-        <Link href="/">
-          <span className="mr-5">Contact</span>
-        </Link>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <Link href="/">
-          <span className="mr-5">Privacy Policy</span>
-        </Link>
-        <Link href="/">
-          <span className="mr-5">Terms of Service</span>
-        </Link>
+    <div className="bg-[#b4aaaa]">
+      <div className="justify-between items-center p-4 max-w-6xl m-auto flex flex-wrap gap-4 mt-10">
+        <div className="logo flex items-start justify-start">
+          <Link href="/">
+            <span className="mr-5">Doggo.dev</span>
+          </Link>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-4">
+          <Link href="/">
+            <span className="mr-5">About</span>
+          </Link>
+          <Link href="/">
+            <span className="mr-5">Contact</span>
+          </Link>
+        </div>
+        <div className="flex flex-col items-start justify-start gap-4">
+          <Link href="/">
+            <span className="mr-5">Privacy Policy</span>
+          </Link>
+          <Link href="/">
+            <span className="mr-5">Terms of Service</span>
+          </Link>
+        </div>
       </div>
     </div>
   )
